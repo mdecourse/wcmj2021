5fbf9c93fc79cf8590103d9f7c4dd39023d98b50
mdecourse <mdecourse@gmail.com>
2021-02-19 16:36:47 +0800

    use local pelican setup

--- local_publishconf.py
+++ local_publishconf.py
@@ -29,8 +29,8 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "kmolab-github"
-DISQUS_DISPLAY_COUNTS = True
+#DISQUS_SITENAME = "kmolab-github"
+#DISQUS_DISPLAY_COUNTS = True
 #GOOGLE_ANALYTICS = ""
 
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
