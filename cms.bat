80a17c82dccb255a7e82947d49efbb9b4659e9f6
mdecourse <mdecourse@gmail.com>
2021-03-25 08:07:43 +0800

    modify cms.bat to allow os.system to open local flask site accordingly

--- cms.bat
+++ cms.bat
@@ -1,1 +1,1 @@
-python cmsimde/wsgi.py
+python cmsimde/wsgi.py 1> nul |  start "C:\Program Files (x86)\Mozilla Firefox\firefox" https://localhost:9443 | start "C:\Program Files\Mozilla Firefox\firefox" https://localhost:9443
